4703f040526783e804e00b532e85efc8aa5853ff
lp100 <1010699685@qq.com>
2017-06-06 16:30:05 +0800

    id生成器

---
+++ zrhx-common/src/main/java/org/com/zrhx/utill/IdGen.java
@@ -0,0 +1,47 @@
+/**
+ * Copyright &copy; 2012-2016 <a href="https://github.com/thinkgem/jeesite">JeeSite</a> All rights reserved.
+ */
+package org.com.zrhx.utill;
+
+import java.io.Serializable;
+import java.security.SecureRandom;
+import java.util.UUID;
+
+import org.apache.shiro.session.Session;
+import org.apache.shiro.session.mgt.eis.SessionIdGenerator;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Service;
+
+/**
+ * 封装各种生成唯一性ID算法的工具类.
+ * @author ThinkGem
+ * @version 2013-01-15
+ */
+public class IdGen implements SessionIdGenerator {
+
+	private static SecureRandom random = new SecureRandom();
+	
+	/**
+	 * 封装JDK自带的UUID, 通过Random数字生成, 中间无-分割.
+	 */
+	public static String uuid() {
+		return UUID.randomUUID().toString().replaceAll("-", "");
+	}
+	
+	/**
+	 * 使用SecureRandom随机生成Long. 
+	 */
+	public static long randomLong() {
+		return Math.abs(random.nextLong());
+	}
+
+
+
+	@Override
+	public Serializable generateId(Session session) {
+		return IdGen.uuid();
+	}
+	
+
+
+}
