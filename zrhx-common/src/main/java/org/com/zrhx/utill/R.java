f6525b74c91f447b5d7e60ba2472cc0fb6c9d8bd
lp100 <1010699685@qq.com>
2017-06-09 14:13:23 +0800

    完善代码

--- zrhx-common/src/main/java/org/com/zrhx/utill/R.java
+++ zrhx-common/src/main/java/org/com/zrhx/utill/R.java
@@ -4,11 +4,10 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * 返回数据
- * 
- * @author chenshun
- * @email sunlightcs@gmail.com
- * @date 2016年10月27日 下午9:59:27
+ * @Title: R
+ * @Description:  返回数据
+ * @author: gs
+ * @date: 2017/6/9 9:39
  */
 public class R extends HashMap<String, Object> {
 	private static final long serialVersionUID = 1L;
--- zrhx-common/src/main/java/org/com/zrhx/utill/RRExceptionHandler.java
+++ zrhx-common/src/main/java/org/com/zrhx/utill/RRExceptionHandler.java
@@ -29,30 +29,53 @@ public class RRExceptionHandler implements HandlerExceptionResolver {
 		if (null != request.getHeader("X-Requested-With") &&
 				request.getHeader("X-Requested-With").equalsIgnoreCase("XMLHttpRequest")) {
 		    //说明该请求为异步请求
-		}
-		R r = new R();
-		try {
-			response.setContentType("application/json;charset=utf-8");
-			response.setCharacterEncoding("utf-8");
-			
-			if (ex instanceof RRException) {
-				r.put("code", ((RRException) ex).getCode());
-				r.put("msg", ((RRException) ex).getMessage());
-			}else if(ex instanceof DuplicateKeyException){
-				r = R.error("数据库中已存在该记录");
-			}else if(ex instanceof AuthorizationException){
-				r = R.error("没有权限，请联系管理员授权");
-			}else{
-				r = R.error();
+
+			try {
+				R r = new R();
+				response.setContentType("application/json;charset=utf-8");
+				response.setCharacterEncoding("utf-8");
+
+				if (ex instanceof RRException) {
+					r.put("code", ((RRException) ex).getCode());
+					r.put("msg", ((RRException) ex).getMessage());
+				}else if(ex instanceof DuplicateKeyException){
+					r = R.error("数据库中已存在该记录");
+				}else if(ex instanceof AuthorizationException){
+					r = R.error("没有权限，请联系管理员授权");
+				}else{
+					r = R.error();
+				}
+
+				//记录异常日志
+				logger.error(ex.getMessage(), ex);
+
+				String json = JSONUtils.toJSONString(r);
+				response.getWriter().print(json);
+			} catch (Exception e) {
+				logger.error("RRExceptionHandler 异常处理失败", e);
 			}
-			
-			//记录异常日志
-			logger.error(ex.getMessage(), ex);
-			
-			String json = JSONUtils.toJSONString(r);
-			response.getWriter().print(json);
-		} catch (Exception e) {
-			logger.error("RRExceptionHandler 异常处理失败", e);
+		}else{
+			ModelAndView modelAndView = new ModelAndView();
+			try {
+				if (ex instanceof RRException) {
+					modelAndView.addObject("message", ((RRException) ex).getMessage());
+					modelAndView.setViewName("error/400.jsp");
+				}else if(ex instanceof DuplicateKeyException){
+					modelAndView.addObject("message", "数据库中已存在该记录");
+					modelAndView.setViewName("error/400.jsp");
+				}else if(ex instanceof AuthorizationException){
+					modelAndView.addObject("message", "没有权限，请联系管理员授权");
+					modelAndView.setViewName("error/400.jsp");
+				}else{
+					modelAndView.addObject("message", "未知异常，请联系管理员");
+					modelAndView.setViewName("error/400.jsp");
+				}
+				//记录异常日志
+				logger.error(ex.getMessage(), ex);
+			} catch (Exception e) {
+				logger.error("RRExceptionHandler 异常处理失败", e);
+			}
+			return modelAndView;
 		}
 		return new ModelAndView();
 	}
