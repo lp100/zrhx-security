445e2d367b10e187f3e8c2edfcf35d9bf5ea9f34
lp100 <1010699685@qq.com>
2017-06-09 11:31:54 +0800

    优化代码

--- zrhx-web/src/main/java/org/com/zrhx/controller/SysPageController.java
+++ zrhx-web/src/main/java/org/com/zrhx/controller/SysPageController.java
@@ -13,10 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class SysPageController {
 	
-	@RequestMapping("sys/{url}.html")
-	public String page(@PathVariable("url") String url){
-		return "sys/" + url + ".html";
-	}
+
 
 	@RequestMapping("{package}/{url}.html")
 	public String generator(@PathVariable("url") String url,@PathVariable("package") String packageName){
