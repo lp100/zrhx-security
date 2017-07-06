0dc315124e98e7ea618846a54753e91136a8b1f8
lp100 <1010699685@qq.com>
2017-07-06 14:27:33 +0800

    增加swagger

--- zrhx-api/src/main/java/org/com/zrhx/config/RestApiConfig.java
+++ zrhx-api/src/main/java/org/com/zrhx/config/RestApiConfig.java
@@ -1,4 +1,5 @@
 package org.com.zrhx.config;
+import io.swagger.annotations.Api;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -7,12 +8,18 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupp
 
 
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Parameter;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @EnableWebMvc
 @EnableSwagger2
@@ -22,11 +29,29 @@ public class RestApiConfig extends WebMvcConfigurationSupport{
 
     @Bean
     public Docket createRestApi() {
+        List<Parameter> operationParameters = new ArrayList<>();
+        Parameter  operationParameter = new ParameterBuilder()
+         .name("token")
+         .description(
+         "密钥")
+         .modelRef(new ModelRef("string"))
+         .parameterType("query").required(true).build();
+
+        Parameter  headeroperationParameter = new ParameterBuilder()
+                .name("headertoken")
+                .description(
+                        "请求头")
+                .modelRef(new ModelRef("string"))
+                .parameterType("header").required(true).build();
+
+        operationParameters.add(operationParameter);
+        operationParameters.add(headeroperationParameter);
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo())
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("org.com.zrhx.api"))
-                .paths(PathSelectors.any())
+                .globalOperationParameters(operationParameters)
+                .select() // 选择那些路径和api会生成document
+                .apis(RequestHandlerSelectors.withClassAnnotation(Api.class))
+                .paths(PathSelectors.any())// 对所有路径进行监控
                 .build();
     }
 
